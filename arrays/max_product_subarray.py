fa3be31e7a610a4a3a3a70c6ae11b7f60c0a25e1
Damrukesh <ddamrukesh@gmail.com>
2026-01-27 22:00:27 +0530

    week1

---
+++ arrays/max_product_subarray.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        maxi=1
+        mini=1
+        gb=nums[0]
+        for n in nums:
+            m=maxi
+            maxi=max(n*maxi,n*mini,n)
+            mini=min(n*m,n*mini,n)
+            gb=max(gb,maxi)
+        return gb
---
+++ arrays/xor_subarray_count.py
@@ -0,0 +1,15 @@
+class Solution:
+    def subarrayXor(self, arr, k):
+        h={}
+        prefix=0
+        h[prefix]=1
+        count=0
+        for n in arr:
+            prefix=prefix^n
+            if prefix^k in h:
+                count+=h[prefix^k]
+            if prefix not in h:
+                h[prefix]=1
+            else:
+                h[prefix]+=1
+        return count         
