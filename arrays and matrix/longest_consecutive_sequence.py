10c81c3f88b60c61bb55025cec5dc127b84494dc
Damrukesh <ddamrukesh@gmail.com>
2026-02-04 20:49:47 +0530

    completed arrays

---
+++ arrays and matrix/longest_consecutive_sequence.py
@@ -0,0 +1,15 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        s=set(nums)
+        longest=0
+
+        for n in s:
+            if n-1 not in s:
+                length=1
+                while n+length in s:
+                    length+=1
+                longest=max(longest,length)
+        return longest        
+
+# Time Complexity: O(N) where N is the number of elements in the input list nums.
+# Space Complexity: O(N) for storing the elements in the set s.     
